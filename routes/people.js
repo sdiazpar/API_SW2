71fc5accb3f15b79c82d98ed5ea435063dfddd32
Pablo Trujillo <pablo.trujillooliva@usp.ceu.es>
2025-05-15 13:42:16 +0200

    reactualizando respuesta post

--- routes/people.js
+++ routes/people.js
@@ -61,7 +61,7 @@ router.post('/', async (req, res) => {
       .collection(COLLECTION)
       .insertOne(newPerson)
       .catch(err => res.status(400).send('Error al insertar la persona'));
-    res.json(result).status(201).send("Persona creada");
+    res.json(result).status(201);
 });
 
 module.exports = router;
